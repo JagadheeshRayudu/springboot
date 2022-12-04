1211c89d553a4a8ac29071783708c83b789e544c
Jagadheesh <jagadheeshrayudu@gmail.com>
2022-12-04 22:18:44 +0530

    Initial commit

---
+++ src/test/java/com/java/spring/swagger/swagger/SwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.spring.swagger.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
